f08b6864eacfdfed1db348efe3f45c9df9b7eb62
rhodinemma <rhodinemma10@gmail.com>
2022-10-16 11:50:56 +0300

    add flutter slider widget

--- lib/icon_content.dart
+++ lib/icon_content.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-var myFontStyle = GoogleFonts.nunito();
-
-const labelTextStyle = TextStyle(
-    fontSize: 18.0,
-    color: Color(0xFF8D8E98)
-);
+import 'constants.dart';
 
 class IconContent extends StatelessWidget {
   final IconData icon;
@@ -23,7 +17,7 @@ class IconContent extends StatelessWidget {
       children: <Widget>[
         Icon(icon, size: 80.0,),
         const SizedBox(height: 15.0,),
-        Text(label, style: labelTextStyle,)
+        Text(label, style: kLabelTextStyle,)
       ],
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter101/reusable_card.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import 'constants.dart';
 import 'icon_content.dart';
-
-const bottomContainerHeight = 80.0;
-const activeCardColor = Color(0xFF1D1E33);
-const inactiveCardColor = Color(0xFF111328);
-const bottomContainerColor = Color(0xFFEB1555);
-var myFontStyle = GoogleFonts.nunito();
 
 enum Gender {
   male,
@@ -50,58 +44,89 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   Gender? selectedGender;
+  int height = 180;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BMI CALCULATOR', style: myFontStyle),
+        title: Text('BMI CALCULATOR', style: kMyFontStyle),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Expanded(
               child: Row(
-            children: <Widget>[
-              Expanded(
-                child: ReusableCard(
-                  onPress: (){
-                    setState(() {
-                      selectedGender = Gender.male;
-                    });
-                  },
-                  colour: selectedGender == Gender.male
-                      ? activeCardColor
-                      : inactiveCardColor,
-                  cardChild: const IconContent(
-                    icon: FontAwesomeIcons.mars,
-                    label: 'Male',
+                children: <Widget>[
+                  Expanded(
+                    child: ReusableCard(
+                      onPress: () {
+                        setState(() {
+                          selectedGender = Gender.male;
+                        });
+                      },
+                      colour: selectedGender == Gender.male
+                          ? kActiveCardColor
+                          : kInactiveCardColor,
+                      cardChild: const IconContent(
+                        icon: FontAwesomeIcons.mars,
+                        label: 'Male',
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Expanded(
-                child: ReusableCard(
-                  onPress: (){
-                    setState(() {
-                      selectedGender = Gender.female;
-                    });
-                  },
-                  colour: selectedGender == Gender.female
-                      ? activeCardColor
-                      : inactiveCardColor,
-                  cardChild: const IconContent(
-                    icon: FontAwesomeIcons.venus,
-                    label: 'Female',
+                  Expanded(
+                    child: ReusableCard(
+                      onPress: () {
+                        setState(() {
+                          selectedGender = Gender.female;
+                        });
+                      },
+                      colour: selectedGender == Gender.female
+                          ? kActiveCardColor
+                          : kInactiveCardColor,
+                      cardChild: const IconContent(
+                        icon: FontAwesomeIcons.venus,
+                        label: 'Female',
+                      ),
+                    ),
                   ),
-                ),
-              ),
-            ],
-          )),
-          /*Expanded(
+                ],
+              )),
+          Expanded(
             child: ReusableCard(
-              colour: activeCardColor,
+              colour: kActiveCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text(
+                    'Height',
+                    style: kLabelTextStyle,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: <Widget>[
+                      Text(height.toString(), style: kNumberTextStyle),
+                      const Text('cm', style: kLabelTextStyle,)
+                    ],
+                  ),
+                  Slider(value: height.toDouble(),
+                      min: 120.0,
+                      max: 220.0,
+                      activeColor: Color(0xFFEB1555),
+                      inactiveColor: Color(0xFF8D8E98),
+                      onChanged: (double newValue) {
+                        setState(() {
+                          height = newValue.round();
+                        });
+                      }),
+                ],
+              ),
+              onPress: () {},
             ),
           ),
-          Expanded(
+          /*Expanded(
             child: Row(
               children: const <Widget>[
                 Expanded(
@@ -118,10 +143,10 @@ class _HomePageState extends State<HomePage> {
             ),
           ),*/
           Container(
-            color: bottomContainerColor,
+            color: kBottomContainerColor,
             margin: const EdgeInsets.only(top: 10.0),
             width: double.infinity,
-            height: bottomContainerHeight,
+            height: kBottomContainerHeight,
           )
         ],
       ),
